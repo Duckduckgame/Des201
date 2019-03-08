ae519348309cbcb37a93bd925ad7f5d77a928f78
Aayush Mathur <aayushmathur14@gmail.com>
2019-03-08 22:46:59 +0000

    Added handle for moving platform's target position

---
+++ Baba Yaga 3D Platformer/Assets/Editor/MovePlatformEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(MovePlatform)), CanEditMultipleObjects]
+public class MovePlatformEditor : Editor
+{
+    protected virtual void OnSceneGUI()
+    {
+        MovePlatform movePlatform = (MovePlatform)target;
+
+        Vector3 initialPosition = movePlatform.transform.TransformPoint(movePlatform.m_initialPosition);
+        Vector3 targetPosition = movePlatform.transform.TransformPoint(movePlatform.localTargetPosition);
+
+        EditorGUI.BeginChangeCheck();
+        Handles.Label(targetPosition, "Target", "button");
+        targetPosition = Handles.PositionHandle(targetPosition, movePlatform.transform.rotation);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(movePlatform, "Move Handles");
+           
+            movePlatform.localTargetPosition = movePlatform.transform.InverseTransformPoint(targetPosition);
+        }
+
+        Debug.Log(targetPosition);
+        
+        Handles.color = Color.yellow;
+        //Handles.DrawLine(initialPosition, targetPosition);
+    }
+
+}
+
+
