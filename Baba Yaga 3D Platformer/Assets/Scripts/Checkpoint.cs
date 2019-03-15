c7d1d75e220e324d7b7e396bf2f9522de5a21d79
Aayush Mathur <aayushmathur14@gmail.com>
2019-03-15 02:20:55 +0000

    Checkpoint functionality added

---
+++ Baba Yaga 3D Platformer/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private PlayerPositon playerPositon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPositon = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPositon>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerPositon.m_reachedPosition = transform.position;
+        }
+    }
+}
---
+++ Baba Yaga 3D Platformer/Assets/Scripts/PlayerPositon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositon : MonoBehaviour
+{
+    public Vector3 m_reachedPosition { get { return reachedPosition; } set { reachedPosition = value; } }
+    private Vector3 reachedPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RespawnTrigger")
+        {
+            transform.position = new Vector3(reachedPosition.x, reachedPosition.y + 1, reachedPosition.z);
+        }
+    }
+}
