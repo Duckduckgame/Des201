3845a84857246c0a6281b075bde4bebab70b038f
Duckduckgame <gabriel.h.kenny@gmail.com>
2019-04-14 18:25:56 +0100

    added ui

--- Baba Yaga 3D Platformer/Assets/startScreenUI.cs
+++ Baba Yaga 3D Platformer/Assets/startScreenUI.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class startScreenUI : MonoBehaviour
 {
+    public Button start;
+    public Button options;
+    public Button quit;
+    public Text music;
+    public Button musicBtn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        start.onClick.AddListener(startClick);
+        options.onClick.AddListener(optionsClick);
+        quit.onClick.AddListener(quitClick);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+       
+
+        
     }
+
+    void startClick() {
+        Debug.Log("starts been clicked");
+        SceneManager.LoadScene("lvlDesTests", LoadSceneMode.Single);
+    }
+    void optionsClick() {
+        Debug.Log("options been clicked");
+    }
+    void quitClick() {
+        Debug.Log("quit been clicked");
+        Application.Quit();
+    }
+    
 }
