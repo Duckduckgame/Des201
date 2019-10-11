f2af847a0b70c7060e5f898aaa96c81ef3af89bd
Elan <46980952+Gamegoy@users.noreply.github.com>
2019-10-11 14:02:36 +0100

    Add files via upload

---
+++ Baba Yaga 3D Platformer/Assets/PlayerAnalyticsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class PlayerAnalyticsManager : MonoBehaviour
+{
+    int noOfPlayerDeaths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    public void PlayerController_OnPlayerDied(Vector3 deathPosition)
+    {
+        noOfPlayerDeaths += 1;
+        Dictionary<string, object> data = new Dictionary<string, object>();
+        data.Add("Position", deathPosition);
+        data.Add("No of Deaths", noOfPlayerDeaths);
+        Analytics.CustomEvent("PLAYER_DIED", data);
+    }
+
+    private void OnDestroy()
+    {
+        Analytics.FlushEvents();
+    }
+
+}
