ba5d5acf7023fc4ed266d8f78b5376e634391312
Elan <46980952+Gamegoy@users.noreply.github.com>
2019-10-14 13:00:49 +0100

    Add files via upload

---
+++ BonusLevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BonusLevelManager : MonoBehaviour
+{
+    [SerializeField]
+    float levelTimer;
+
+    public Text timerText;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        levelTimer = 60.0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        levelTimer -= Time.deltaTime;
+        timerText.text = (levelTimer).ToString("0");
+        if(levelTimer < 0)
+        {
+            TimerEnded();
+          
+        }
+    }
+    void TimerEnded()
+    {
+        SceneManager.LoadScene("lvlDesTests", LoadSceneMode.Single);
+    }
+}
